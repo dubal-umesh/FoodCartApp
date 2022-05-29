ae515f27e653abbcd2fa77173672f8dc724ba0aa
DUBAL <DUBALU@pfizer.com>
2022-05-29 10:06:03 +0530

    Code updated

---
+++ FoodCourtApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodCourtApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public float Rate { get; set; }
+        public string ProductLogoPath { get; set; }
+    }
+}
